f0ec71761276df624f84256c97f6cacd81084912
Rio Chandra Rajagukguk <riochr17@gmail.com>
2017-11-11 17:01:42 +0700

    integrasi login

--- application/controllers/client.php
+++ application/controllers/client.php
@@ -9,14 +9,14 @@ class Client extends CI_Controller {
 	}
 	
 	public function index(){
-        $data['username'] = $this->session->userdata('username');
-        $data['nama'] = $this->session->userdata('nama');
+        $data['username'] = $this->session->userdata('email');
+        $data['nama'] = $this->session->userdata('id');
 		$this->load->view('page/client');
 	}
     
     private function check_isvalidated()
 	{
-        if(! $this->session->userdata('validated'))
+        if(! $this->session->userdata('id'))
 		{
             redirect('login','refresh');
         }
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -11,27 +11,48 @@ class Login extends CI_Controller {
 		$this->load->view('temp/head');
         $data['msg'] = $msg;
         $this->load->view('login_view', $data);
+    	$this->load->library('session');
     }
 	
 	public function process()
-	{        
-		$this->load->model('login_model');
-        $result = $this->login_model->validate();
-        if(! $result)
+	{    
+        $result = $this->RESTLogin($this->input->post('email'), $this->input->post('password'));
+		$this->session->set_userdata($result);
+
+        if($result->error)
 		{
-            $msg = '<br/><i>Invalid email and/or password.</i><br />';
-            $this->index($msg);
+            $this->index($result->message);
         }
 		else
 		{
-//            echo "masuk";
             redirect('client');
-        }        
+        }
     }	
     
     public function do_logout()
 	{
         $this->session->sess_destroy();
         redirect('login','refresh');
+    }
+
+    public function RESTLogin($email, $password){
+        $url = 'http://happyscope.co:3002/login';
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-Type: application/json',
+                'content' => json_encode([
+                	'email' 	=> $email,
+                	'password' 	=> $password
+                ])
+            )
+        );
+        $context  = stream_context_create($opts);
+        $result = @file_get_contents($url, false, $context);
+        if (strpos($http_response_header[0], '200') === false) {
+            return false;
+        }
+        $data = json_decode($result);
+        return $data;
     }
 }
