4ad68015762418417c25187d19d489688b861412
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-02-06 18:07:16 +0700

    test

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'happysco_user';
-$db['default']['password'] = 'masukdatabase';
-$db['default']['database'] = 'happysco_database';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'hsc';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
