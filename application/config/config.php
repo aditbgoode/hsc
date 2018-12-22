f570cbdb843d6803c760dfa78973c11e14c599c8
Rio Chandra Rajagukguk <riochr17@gmail.com>
2018-12-22 11:33:49 +0700

    perbaikan route url server

--- application/config/config.php
+++ application/config/config.php
@@ -22,10 +22,10 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://happyscope.co';
 $http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
 $newurl = str_replace("index.php","", $_SERVER['SCRIPT_NAME']);
-$config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
+// $config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] . "" . $newurl;
 
 /*
 |--------------------------------------------------------------------------
